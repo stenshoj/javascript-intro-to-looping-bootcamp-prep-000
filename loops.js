10d80fc7184ca658c7278805675e9d531f1fc6e7
Christoffer Stenshøj <c.stenshoj@gmail.com>
2017-02-14 12:06:42 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,22 @@
+function forLoop(array) {
+  for(var i = 0; i < 25; i++){
+    array.push(`I am ${i+1} strange loop${i === 0 ? '' : 's'}.`);
+  }
+  return array;
+}
+function whileLoop(n) {
+  while(n != 0) {
+    console.log(n);
+    n--;
+  }
+  return 'done';
+}
+function maybeTrue() {
+  return Math.random() >= 0.5 // Returns a random number between 0 (inclusive) and 1 (exclusive)
+}
+function doWhileLoop(array) {
+  do {
+    array.pop();
+  } while(maybeTrue() && array.length > 0);
+  return array;
+}
